1119a12631243760adf53fb8596589ea28f09ee4
bobug132 <45463032+bobug132@users.noreply.github.com>
2018-11-30 01:05:46 +0800

    add setting class
    add setting class

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,23 +1,24 @@
 import sys
 import pygame
+from setting import Settings
 
 def run_game():
-    #初始化游戏并创建一个屏幕对象
+    #init game and screen object
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    bg_color = (80,80,80)
-    #开始游戏的主循环
+    #start main loop
     while True:
 
-        #监视鼠标和键盘事件
+        #watch mouse、key..event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        screen.fill(bg_color)
-        #让最近绘制的屏幕可见
+        screen.fill(ai_settings.bg_color)
+        #refresh screen
         pygame.display.flip()
 
 
---
+++ setting.py
@@ -0,0 +1,7 @@
+class Settings(object):
+    """docstring forSettings."""
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (80,80,80)
+        
