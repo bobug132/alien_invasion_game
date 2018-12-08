e2a1a87191f06601c90e06ff93bc06414425471d
bobug132 <45463032+bobug132@users.noreply.github.com>
2018-12-08 22:17:04 +0800

    ship modify
    ship is ok

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
-import sys
 import pygame
+from pygame.sprite import Group
 from setting import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     #init game and screen object
@@ -8,18 +10,14 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen, ai_settings)
+    bullets = Group()
 
     #start main loop
     while True:
-
         #watch mouse、key..event
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        screen.fill(ai_settings.bg_color)
-        #refresh screen
-        pygame.display.flip()
-
+        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
---
+++ ship.py
@@ -0,0 +1,30 @@
+import pygame
+
+class Ship():
+    """docstring forShip."""
+    def __init__(self, screen, ai_settings):
+        self.screen = screen
+        self.ai_settings = ai_settings
+        self.moving_right = False
+        self.moving_left = False
+
+        #load ship image
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        #position of Ship
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+        self.center = float(self.rect.centerx)
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        if self.moving_right and self.rect.centerx < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.centerx > self.screen_rect.left:
+            self.center -= self.ai_settings.ship_speed_factor
+        self.rect.centerx = self.center
+        #print(self.rect.centerx)
